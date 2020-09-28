a111ade02cb2fdc2c245b493a9990432dadfb8f1
coalpha <22552895+coalpha@users.noreply.github.com>
2020-09-28 03:00:39 -0700

    5.22
    I think 6.22 is done and I know how I'm gonna design 5.22 so
    perhaps I should get some sleep before trading hours today.
    Alergies were pretty bad around here, I must say.

--- labs/5.22/ComputeLoan.java
+++ labs/5.22/ComputeLoan.java
@@ -1,21 +1,50 @@
+import java.util.Iterator;
 import java.util.Scanner;
+import static java.lang.System.out;
+
+abstract class LoanPayment {
+   public short paymentNumber;
+   public short paymentsRemaining;
+
+   public double interestPaid;
+   public double principalPaid;
+
+   public double totalPaid;
+   public double remainingBalance;
+}
+
+abstract class Loan implements Iterable<LoanPayment> {
+   public final double principal;
+   public final double monthlyRate;
+   public final short months;
+
+
+   public Loan(double principal, double monthlyRate, short months) {
+      this.principal = principal;
+      this.monthlyRate = monthlyRate;
+      this.months = months;
+   }
+
+   @Override
+   Iterator<LoanPayment> iterator() {
+      return new Iterator<LoanPayment>(){
+         
+      };
+   }
+}
 
 public class ComputeLoan {
+   private static final Scanner sc = new Scanner(System.in);
+
    public static void main(String[] dummy) {
-      double loanAmount = intPrompt("Loan Amount: ");
-      var yearsLoanTime = intPrompt("Number of Years: ");
-      var yearlyInterestRate = intPrompt("Annual Interest Rate: ");
+      out.print("Loan amount: ");
+      var loanAmount = sc.nextDouble();
+      var yearsLoanTime = sc.nextShort();
+      var yearlyInterestRate = sc.nextDouble();
       sc.close();
 
       var monthsLoanTime = yearsLoanTime * 12;
       var monthlyInterestRate = yearlyInterestRate / 12;
 
-   }
-
-   private static Scanner sc = new Scanner(System.in);
-
-   private static int intPrompt(String prompt) {
-      System.out.print(prompt);
-      return ComputeLoan.sc.nextInt();
    }
 }
