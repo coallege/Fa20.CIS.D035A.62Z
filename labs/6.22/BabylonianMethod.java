5e86a161b1bd04d6093703a4f36d510100bd4ce7
coalpha <22552895+coalpha@users.noreply.github.com>
2020-09-28 01:19:10 -0700

    goodenuf

--- labs/6.22/BabylonianMethod.java
+++ labs/6.22/BabylonianMethod.java
@@ -1,5 +1,38 @@
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class BabylonianMethod {
    public static double tolerance = 0.0001;
+
+   /** clears the terminal */
+   static void clear() {
+      System.out.print("\033[3J\033[H\033[2J");
+   }
+   public static void main(String[] dummy) {
+      clear();
+      System.out.println(""
+         + "BabylonianMethod.java\n"
+         + "Author: Cole Gannon\n"
+         + "---------------------"
+      );
+      var sc = new Scanner(System.in);
+      while (true) {
+         System.out.print(  "in      : ");
+
+         int in = 0;
+         try {
+            in = sc.nextInt();
+         } catch (NoSuchElementException e) {
+            sc.close();
+            System.out.println("\nInterrupt. Exiting...");
+            System.exit(0);
+         }
+
+         System.out.println("builtin : " + Math.sqrt(in));
+         System.out.println("sqrt    : " + sqrt(in));
+         System.out.printf( "diff    : %.16f\n\n", Math.abs(Math.sqrt(in) - sqrt(in)));
+      }
+   }
 
    /**
     * Get the square root of a number using a trampoline'd version of the
@@ -7,7 +40,8 @@ public class BabylonianMethod {
     */
    public static double sqrt(long n) {
       double old;
-      double next = n / 2; // could be any positive number but this is an okay start
+      // double next = n / 2; // could be any positive number but this is an okay start
+      double next = 1;
       do {
          old = next;
          next = (old + n / old) / 2;
@@ -26,7 +60,7 @@ public class BabylonianMethod {
    public static double sqrt_r(long n, double initialGuess) {
       double nextGuess = (initialGuess + n / initialGuess) / 2;
       if (Math.abs(nextGuess - initialGuess) > tolerance) {
-         return apprxSqrt(n, nextGuess);
+         return sqrt_r(n, nextGuess);
       } else {
          return nextGuess;
       }
